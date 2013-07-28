b138c07b7568a94aacf2009af9ca8d5c48a7a354
Ekramvis <ekramvis@gmail.com>
2013-07-28 11:35:53 -0700

    Update mass_object.rb

--- lib/active_record_lite/mass_object.rb
+++ lib/active_record_lite/mass_object.rb
@@ -1,35 +1,35 @@
 class MassObject
 
-	def self.new(p = {})
-		super
-	end
+  def self.new(p = {})
+    super
+  end
 
-	def initialize(p = {})
-		p.each do |attr, val|
-			if self.class.attributes.include?(attr.to_sym)
-				self.send("#{attr}=", val)
-			else
-				raise "mass assignment to unregistered attribute #{attr}"
-			end
-		end
-	end
+  def initialize(p = {})
+    p.each do |attr, val|
+      if self.class.attributes.include?(attr.to_sym)
+        self.send("#{attr}=", val)
+      else
+	raise "mass assignment to unregistered attribute #{attr}"
+      end
+    end
+  end
 
-	def self.set_attrs(*attributes)
-		@attributes = attributes
+  def self.set_attrs(*attributes)
+    @attributes = attributes
 
-		attributes.each do |attr|
-			self.send(:attr_accessor, attr)
-		end
-	end
+    attributes.each do |attr|
+      self.send(:attr_accessor, attr)
+    end
+  end
 
-	def self.attributes
-		@attributes
-	end
-
+  def self.attributes
+    @attributes
+  end
 end
 
+
 class MyClass < MassObject
-	set_attrs :x, :y
+  set_attrs :x, :y
 end
 
 
